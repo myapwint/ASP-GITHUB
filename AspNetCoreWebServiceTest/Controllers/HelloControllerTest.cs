a9a26e77cced90eab0f40efba70647f35b231b4e
Mya Pwint <zayyadipah@gmail.com>
2023-04-08 17:05:46 +0800

    code fixed

--- AspNetCoreWebServiceTest/Controllers/HelloControllerTest.cs
+++ AspNetCoreWebServiceTest/Controllers/HelloControllerTest.cs
@@ -10,8 +10,7 @@ namespace AspNetCoreWebServiceTest.Controllers
         {
             HelloController controller = new HelloController();
             var response = controller.Get().Value as Response;
-            // Assert.Equal("Hello World!", response.Output);
-            Assert.Equal("Hello World!!!", response.output);
+            Assert.Equal("Hello World!", response.Output);
         }
 
         [Theory]
